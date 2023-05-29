c5c7f8e86691bfcf72158d9db42e464497697dd4
AysKrimn <akabilmemne@gmail.com>
2023-05-29 19:43:30 +0300

    .

---
+++ userApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# Register your models here.
+from .models import *
+
+admin.site.register(NetflixUser)
+admin.site.register(Categories)
+admin.site.register(Type)
+admin.site.register(Movies)
+admin.site.register(NetflixProfile)

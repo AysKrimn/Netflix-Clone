8ecf93d220537233228c83bee60efc860f1d7136
_omerock <83617943+AysKrimn@users.noreply.github.com>
2024-01-11 23:06:07 +0300

    Delete userApp directory

--- userApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import *
-
-admin.site.register(NetflixUser)
-admin.site.register(Categories)
-admin.site.register(Type)
-admin.site.register(Movies)
-admin.site.register(NetflixProfile)
